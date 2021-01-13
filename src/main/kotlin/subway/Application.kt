255c9263baa0c9d3ff1563fa17207abd2b43f7f4
Joowon <joowon@Joowonui-MacBookAir.local>
2021-01-13 23:04:25 +0900

    [feat] station, line 초기상태 init

--- src/main/kotlin/subway/Application.kt
+++ src/main/kotlin/subway/Application.kt
@@ -1,5 +1,27 @@
 package subway
 
+import subway.domain.Line
+import subway.domain.LineRepository.addLine
+
+import subway.domain.Station
+import subway.domain.StationRepository.addStation
+import subway.view.mainPage
+
+
 fun main() {
-    // TODO: 프로그램 구현
+    init()
+    mainPage()
+}
+
+fun init() {
+    addStation(Station("교대역"))
+    addStation(Station("강남역"))
+    addStation(Station("역삼역"))
+    addStation(Station("남부터미널역"))
+    addStation(Station("양재역"))
+    addStation(Station("양재시민의숲역"))
+    addStation(Station("매봉역"))
+    addLine(Line("2호선"))
+    addLine(Line("3호선"))
+    addLine(Line("4호선"))
 }
