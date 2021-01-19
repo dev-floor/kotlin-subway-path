815093c69573559b43193201bf7dbf1db6d112d1
hungryjay <juwonson2068@naver.com>
2021-01-19 18:34:12 +0900

    [feat] 구간 삭제 view

--- src/main/kotlin/subway/view/DeleteSectionView.kt
+++ src/main/kotlin/subway/view/DeleteSectionView.kt
@@ -1,1 +1,16 @@
 package subway.view
+
+fun getLineNameOfSectionToDelete(): String {
+    println("\n## 삭제할 구간의 노선을 입력하세요.")
+    return readLine()!!
+}
+
+fun getUpwardNameOfSectionToDelete(): String {
+    println("\n## 삭제할 구간의 상행역을 입력하세요.")
+    return readLine()!!
+}
+
+fun getDownwardNameOfSectionToDelete(): String {
+    println("\n## 삭제할 구간의 하행역을 입력하세요.")
+    return readLine()!!
+}
