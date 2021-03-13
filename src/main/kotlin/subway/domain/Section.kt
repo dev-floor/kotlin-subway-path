3f70e95db9093f42436a002549ea4e5348efc457
hungryjay <juwonson2068@naver.com>
2021-03-13 15:57:19 +0900

    refactor: default값 변수화

--- src/main/kotlin/subway/domain/Section.kt
+++ src/main/kotlin/subway/domain/Section.kt
@@ -4,11 +4,15 @@ data class Section(
     val line: Line,
     val upwardStation: Station,
     val downwardStation: Station,
-    var time: Int? = 3,
-    var distance: Int? = 2
+    var time: Int? = TIME_DEFAULT,
+    var distance: Int? = DISTANCE_DEFAULT
 ) {
     fun matchUpward(name: String): Boolean = name == this.upwardStation.name
 
     fun matchDownward(name: String): Boolean = name == this.downwardStation.name
 
+    companion object {
+        const val TIME_DEFAULT = 3
+        const val DISTANCE_DEFAULT = 2
+    }
 }
